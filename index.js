2f1828761761849317855f7c8ec6195ece32cfa7
Rinat Sagitov <sagitovrf@gmail.com>
2020-10-12 00:23:53 +0500

    lesson 3 + cloud mognodb

--- index.js
+++ index.js
@@ -1,42 +1,59 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_CONN, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Product = mongoose.model('Product', {
+	id: Number,
+	name: String,
+	price: mongoose.Schema.Types.Decimal128,
+});
 
 const app = express();
 app.use(bodyParser.json());
 
-const products = [
-	{
-		id: 1,
-		name: 'phone',
-		price: 300,
-	},
-	{
-		id: 2,
-		name: 'tablet',
-		price: 700,
-	},
-];
+const message = `Hi, it's my first test api!
+Use Postman for send requests, please.
+Available routes /product & /products/id
+Methods GET POST PUT DELETE
+Object Product
+{
+	id: Number,
+	name: String,
+	price: mongoose.Schema.Types.Decimal128,
+}
+`
 
-app.get('/', (req, res) => res.send('Hi, World!'));
+app.get('/', (req, res) => res.send(message));
 
-app.get('/products', (req, res) => res.json(products));
-app.post('/products', (req, res) => {
-	products.push(req.body);
-	res.json(req.body);
-});
-app.put('/products/:id', (req, res) => {
-	const product = products.find(p => p.id === +req.params.id);
-	const productIndex = products.indexOf(product);
-	const newProduct = { ...product, ...req.body };
-	products[productIndex] = newProduct;
-	res.json(newProduct);
-});
-app.delete('/products/:id', (req, res) => {
-	const product = products.find(p => p.id === +req.params.id);
-	const productIndex = products.indexOf(product);
-	products.splice(productIndex, 1);
-	res.json({ success: true });
-});
+app.get('/products',
+	(req, res) => {
+		Product.find()
+			.exec()
+			.then(products => res.json(products))
+	});
+
+app.post('/products',
+	(req, res) => {
+		Product.create(req.body)
+			.then(createPoduct => res.json(createPoduct))
+	});
+
+app.put('/products/:id',
+	(req, res) => {
+		Product.findOneAndUpdate({ id: req.params.id }, req.body)
+			.exec()
+			.then(product => res.json(product))
+	});
+
+app.delete('/products/:id',
+	(req, res) => {
+		Product.deleteOne({ id: req.params.id })
+			.exec()
+			.then(() => res.json({ success: true }))
+	});
 
 // eslint-disable-next-line
 app.listen(3000, () => console.log('Listenig on port 3000...'));
